2578342d500c135218b0c2dcc0b801c313f3564d
Jonathan Arnett <jonarnett90@gmail.com>
2021-09-29 10:12:48 -0400

    Take 2

--- lib/tacit.ex
+++ lib/tacit.ex
@@ -9,38 +9,22 @@ defmodule Tacit do
   ## Examples
 
     iex> import Tacit
-    ...> let foo = String.join <|> Enum.reverse
+    ...> let foo = Enum.reverse |> String.join
     ...> foo([1,2,3])
     "321"
   """
   defmacro let({:=, _, [{name, _, nil}, definition]}) do
     quote do
-      def(unquote(name)(arg), do: unquote(translate(definition, quote(do: arg))))
+      def(unquote(name)(arg), do: arg |> unquote(definition))
     end
   end
 
   defmacro let(_) do
     raise """
-    Could not identify the name of the definition. Please define tacit functions
-    in the following style:
+    Could not parse the definition. Please define tacit functions in the
+    following style:
 
-        let foo = fun1 . fun2 . fun3
-
-    Parentheses are not supported.
+        let foo = fun1 |> fun2 |> fun3
     """
-  end
-
-  defp translate({:<|>, _meta1, [tail, {head, _meta2, empty}]}, arg) when empty in [nil, []] do
-    translate(tail, compose(head, arg))
-  end
-
-  defp translate({name, _meta, _args}, arg) do
-    compose(name, arg)
-  end
-
-  defp compose(a, b) do
-    quote do
-      unquote(a)(unquote(b))
-    end
   end
 end
--- test/tacit_test.exs
+++ test/tacit_test.exs
@@ -3,11 +3,33 @@ defmodule TacitTest do
 
   defmodule Example do
     import Tacit
+    import String, only: [capitalize: 1]
 
-    let foo = Enum.reverse <|> String.split
+    let remote =
+          String.split()
+          |> Enum.reverse()
+
+    let anonymous = then(&String.split(&1, ",")) |> Enum.reverse()
+    let imported = capitalize
+    let local = get_age |> add_one
+
+    defp get_age(%{age: age}), do: age
+    defp add_one(arg), do: arg + 1
   end
 
-  test "it works" do
-    assert Example.foo("1 2 3") == ["3", "2", "1"]
+  test "handles remote functions" do
+    assert Example.remote("1 2 3") == ["3", "2", "1"]
+  end
+
+  test "handles anonymous functions" do
+    assert Example.anonymous("1,2,3") == ["3", "2", "1"]
+  end
+
+  test "handles imported functions" do
+    assert Example.imported("hello") == "Hello"
+  end
+
+  test "handles local functions" do
+    assert Example.local(%{name: "J3RN", age: 30}) == 31
   end
 end
